3d3542e6d2c02f8a1c0228f72f00fd2a685ee2f2
dantevlou <193077542+dantevlou@users.noreply.github.com>
2025-03-14 03:01:18 +1100

    Added display screen

--- calculator.py
+++ calculator.py
@@ -3,7 +3,11 @@ import tkinter as tk
 # Main window
 root = tk.Tk()
 root.title("Calculator")
-root.geometry("300x400")
+root.geometry("415x500")
+
+# Display screen
+entry = tk.Entry(root, width=21, font=("Arial", 24), borderwidth=2, relief="solid")
+entry.grid(row=0, column=0, columnspan=4, padx=14, pady=14)
 
 # Runs the application
 root.mainloop()
